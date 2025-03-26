ec572471e6cdd5aaa057e2bd60a70b9ff9f2e1bd
ZeroToHero2k1 <toquyphuoc12a9@gmail.com>
2025-03-26 15:19:53 +0700

    thêm hệ cho các pokemon

--- js/pokemonapi.js
+++ js/pokemonapi.js
@@ -1,4 +1,8 @@
 document.getElementById("searchPokemon").addEventListener("click",function(){
+    const myDiv = document.getElementById("pokemonOutput");
+    while (myDiv.firstChild) {
+        myDiv.removeChild(myDiv.firstChild);
+    }
     PokedexByPhuoc();
 })
 async function PokedexByPhuoc(){
@@ -9,13 +13,36 @@ async function PokedexByPhuoc(){
             alert("Không có Pokemon này trong Pokedex, vui lòng nhập đúng tên");
         }
         const data=await pokeapi.json();
-        console.log(data);
-
+        // console.log(data);
+        //Lất ảnh Pokemon
         const pokemonOutput=data.sprites.front_default;
         const imgOfPokemon=document.getElementById("imageOfPokemon");
         imgOfPokemon.src=pokemonOutput;
         imgOfPokemon.style.display="block";
-
+        //Lấy hệ của Pokemon
+        let typePoke=new Array();
+        typePoke=data.types;
+        const outPutPokemonDiv=document.getElementById("pokemonOutput");
+        for(let i=0;i<=typePoke.length-1;i++){
+            const newParagraph  = document.createElement("p");
+            const textNode = document.createTextNode(typePoke[i].type.name);
+            newParagraph.appendChild(textNode);
+            newParagraph.style.color="white";
+            if(textNode.textContent==="fire"){               
+                newParagraph.style.backgroundColor="red";
+            }
+            else if(textNode.textContent==="normal"){
+                newParagraph.style.backgroundColor="grey";
+            }
+            else if(textNode.textContent==="fighting"){
+                newParagraph.style.backgroundColor="orange";
+            }
+            else if(textNode.textContent==="water"){
+                newParagraph.style.backgroundColor="blue";
+            }
+            
+            outPutPokemonDiv.appendChild(newParagraph);
+        }
     }
     catch(error){
         console.log(error);
