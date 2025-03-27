03c9f3cce8156217ccff7e5ddf0cb86de2bccd3e
ZeroToHero2k1 <toquyphuoc12a9@gmail.com>
2025-03-27 10:50:25 +0700

    sửa responsive và pokemon api

--- js/pokemonapi.js
+++ js/pokemonapi.js
@@ -19,6 +19,8 @@ async function PokedexByPhuoc(){
         const imgOfPokemon=document.getElementById("imageOfPokemon");
         imgOfPokemon.src=pokemonOutput;
         imgOfPokemon.style.display="block";
+        imgOfPokemon.style.width="300px";
+        imgOfPokemon.style.height="fit-content";
         //Lấy hệ của Pokemon
         let typePoke=new Array();
         typePoke=data.types;
@@ -28,18 +30,64 @@ async function PokedexByPhuoc(){
             const textNode = document.createTextNode(typePoke[i].type.name);
             newParagraph.appendChild(textNode);
             newParagraph.style.color="white";
-            if(textNode.textContent==="fire"){               
-                newParagraph.style.backgroundColor="red";
-            }
-            else if(textNode.textContent==="normal"){
+            newParagraph.style.width="fit-content";
+            if(textNode.textContent==="normal"){               
                 newParagraph.style.backgroundColor="grey";
             }
-            else if(textNode.textContent==="fighting"){
-                newParagraph.style.backgroundColor="orange";
+            else if(textNode.textContent==="fire"){
+                newParagraph.style.backgroundColor="red";
             }
             else if(textNode.textContent==="water"){
                 newParagraph.style.backgroundColor="blue";
             }
+            else if(textNode.textContent==="electric"){
+                newParagraph.style.backgroundColor="gold";
+            }
+            else if(textNode.textContent==="grass"){
+                newParagraph.style.backgroundColor="green";
+            }
+            else if(textNode.textContent==="ice"){
+                newParagraph.style.backgroundColor="whiteblue";
+            }
+            else if(textNode.textContent==="fighting"){
+                newParagraph.style.backgroundColor="brown";
+            }
+            else if(textNode.textContent==="poison"){
+                newParagraph.style.backgroundColor="violet";
+            }
+            else if(textNode.textContent==="ground"){
+                newParagraph.style.backgroundColor="#eab676";
+            }
+            else if(textNode.textContent==="flying"){
+                newParagraph.style.backgroundColor="#7cd2e3";
+            }
+            else if(textNode.textContent==="psychic"){
+                newParagraph.style.backgroundColor="pink";
+            }
+            else if(textNode.textContent==="bug"){
+                newParagraph.style.backgroundColor="#c1e37c";
+            }
+            else if(textNode.textContent==="rock"){
+                newParagraph.style.backgroundColor="#cbc55c";
+            }
+            else if(textNode.textContent==="ghost"){
+                newParagraph.style.backgroundColor="#9a1ebf";
+            }
+            else if(textNode.textContent==="dragon"){
+                newParagraph.style.backgroundColor="#2651b9";
+            }
+            else if(textNode.textContent==="dark"){
+                newParagraph.style.backgroundColor="#7b442a";
+            }
+            else if(textNode.textContent==="steel"){
+                newParagraph.style.backgroundColor="gray";
+            }
+            else if(textNode.textContent==="fairy"){
+                newParagraph.style.backgroundColor="#f1bee7";
+            }
+            newParagraph.style.marginLeft="3px";
+            newParagraph.style.padding="3px";
+            newParagraph.style.borderRadius="10px"
             
             outPutPokemonDiv.appendChild(newParagraph);
         }
@@ -49,3 +97,10 @@ async function PokedexByPhuoc(){
     }
 
 }
+//khá hay đấy, để tạo mã nhân viên hoặc tính giờ phút dây dưới 10 thì sẽ có thêm số 0 ở đằng trước
+//2 ở đây quy định String này chỉ được phép có 2 kí tự
+const now=new Date();
+console.log(String(now.getSeconds()).padStart(2,"0"));
+//hasAttribute()
+const a=document.getElementById("imageOfPokemon")
+console.log(a.hasAttribute("style"));
